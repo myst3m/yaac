f23a7f293021e31fe1558a637f50fd8fac8191f8
tmiya4ta <tmiyashita@salesforce.com>
2024-03-21 15:58:45 +0900

    Added test items

--- test/yaac/core_test.clj
+++ test/yaac/core_test.clj
@@ -1,21 +1,36 @@
 (ns yaac.core-test
-  (:require [yaac.core :as sut]
+  (:require [yaac.core :as yc]
+            [yaac.login :as yl]
+            [silvur.log :as log]
             [clojure.test :as t :refer (deftest is testing use-fixtures are)]))
 
 
+;; Required to store credentials to .yaac/credentials like below
+;; {"c2":	
+;;  {"client_id":"your id",
+;;   "client_secret":"your secret",
+;;   "grant_type":"client_credentials"}}
+ 
 (defn login-fixture [f]
-  (sut/login "c2")
+  (log/set-min-level! :warn)
+  (yl/login {:args ["c2"]})
   (f))
+
+(use-fixtures :once login-fixture)
 
 (deftest login-test
   (testing "login"
-    (is (some? (:access-token sut/default-credential)))))
+    (is (some? (:access-token yc/default-credential)))))
 
 (deftest get-test
   (testing "get org"
-    (is (map? (first (sut/get-organizations)))))
+    (is (map? (first (yc/get-organizations)))))
   (testing "get environment"
-    (is (map? (first (sut/get-environments "T1")))))
-  (testing "get asset"
-))
+    (is (map? (first (yc/get-environments {:args ["T1"]})))))
+  (testing "get app"
+    (let [{:keys [id extra]} (first (yc/get-deployed-applications {:args ["T1"  "Production"]}))]
+      (is (= #{:org :env :status :target} (set (keys extra))))))
+  (testing "get api instances"
+    (let [{:keys [id extra]} (first (yc/get-api-instances {:args ["T1" "Production"]}))]
+      (is (= #{:org :env :status :target} (set (keys extra)))))))
 
