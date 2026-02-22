98330660e08a3bffde48c9e4d2cbc66d094038e3
Tsutomu MIYASHITA <myst3m@gmail.com>
2026-02-23 01:48:03 +0900

    Add generic OAuth2 auth flows: code, client, OIDC discovery, -p preset
    - yaac auth code: Authorization Code Flow (generic, replaces azure-only)
    - yaac auth client: Client Credentials Flow
    - OIDC discovery via issuer=<url> (.well-known/openid-configuration)
    - -p azure preset for Azure Entra (backward compatible)
    - yaac auth azure kept as legacy shorthand

--- src/yaac/auth.clj
+++ src/yaac/auth.clj
@@ -8,140 +8,239 @@
 
 (ns yaac.auth
   (:gen-class)
-  (:import [java.util.zip ZipInputStream ZipEntry]
-           [java.io FileInputStream]
-           [java.nio.charset Charset])
-  (:require [clojure.spec.alpha :as s]
-            [clojure.tools.cli :refer (parse-opts)]
-            [clojure.zip :as z]
-            [clojure.tools.cli :refer [parse-opts]]
-            [muuntaja.core :as m]
-            [clojure.string :as str]
-            [clojure.java.io :as io]
-            [taoensso.nippy :as nippy]
+  (:require [clojure.string :as str]
             [zeph.client :as http]
-            [silvur.nio :as nio]
-            [camel-snake-kebab.core :as csk]
-            [camel-snake-kebab.extras :as cske]
             [yaac.util :refer [json->edn edn->json]]
             [taoensso.timbre :as log]
             [zeph.server :refer [run-server]]
-            [jsonista.core :as json]
             [yaac.util :as util]
             [yaac.error :as e]
-            [clojure.data.xml :as dx]
-            [clojure.set :as set]
-            [yaac.yaml :as yaml]
-            [clojure.core.async :as async :refer [go <!! chan >!!]]
+            [clojure.core.async :as async :refer [<!! chan >!!]]
             [yaac.core :as yc]
             [reitit.http :as rh]
             [reitit.http.interceptors.parameters]
-            [reitit.http.interceptors.muuntaja]
             ))
 
 (defn- url-encode [s]
   (java.net.URLEncoder/encode (str s) "UTF-8"))
 
+;; --- OIDC Discovery ---
+
+(defn- discover-oidc
+  "Fetch .well-known/openid-configuration from issuer URL"
+  [issuer]
+  (let [url (str (str/replace issuer #"/$" "") "/.well-known/openid-configuration")]
+    (log/debug "OIDC discovery:" url)
+    (-> @(http/get url {})
+        :body
+        (json->edn))))
+
+;; --- Presets ---
+
+(def presets
+  {"azure" {:authorize-url-fn (fn [{:keys [tenant]}]
+                                (str "https://login.microsoftonline.com/" tenant "/oauth2/v2.0/authorize"))
+            :token-url-fn     (fn [{:keys [tenant]}]
+                                (str "https://login.microsoftonline.com/" tenant "/oauth2/v2.0/token"))
+            :default-scope    "https://graph.microsoft.com/.default"
+            :extra-params     [:tenant]}})
+
+(defn- resolve-endpoints
+  "Resolve authorize-url and token-url from preset, issuer, or explicit params"
+  [{:keys [preset issuer authorize-url token-url] :as params}]
+  (cond
+    ;; Explicit URLs
+    (and authorize-url token-url)
+    {:authorize-url (first authorize-url)
+     :token-url (first token-url)}
+
+    ;; Preset
+    preset
+    (let [p (get presets (first preset))]
+      (when-not p
+        (throw (e/invalid-arguments "Unknown preset" {:preset (first preset)
+                                                      :available (keys presets)})))
+      {:authorize-url ((:authorize-url-fn p) (update-vals params #(first %)))
+       :token-url ((:token-url-fn p) (update-vals params #(first %)))
+       :default-scope (:default-scope p)})
+
+    ;; OIDC Discovery
+    issuer
+    (let [oidc (discover-oidc (first issuer))]
+      {:authorize-url (:authorization-endpoint oidc)
+       :token-url (:token-endpoint oidc)})
+
+    :else nil))
+
+;; --- Callback server for Authorization Code Flow ---
+
+(defn- callback-app [token-url client-id client-secret redirect-uri scope pipe]
+  (rh/ring-handler
+   (rh/router [["/oauth2/callback" {:get (fn [{:keys [params]}]
+                                           (let [{:strs [code state]} params]
+                                             (-> @(http/post token-url
+                                                            {:form-params {:client_id client-id
+                                                                           :scope scope
+                                                                           :code code
+                                                                           :redirect_uri redirect-uri
+                                                                           :grant_type "authorization_code"
+                                                                           :client_secret client-secret}})
+                                                 :body
+                                                 (json->edn)
+                                                 (as-> x (do (>!! pipe x) x)
+                                                   (assoc {:status 200} :body (edn->json :snake x))))))}]])
+   {:executor reitit.interceptor.sieppari/executor
+    :interceptors [(reitit.http.interceptors.parameters/parameters-interceptor)]}))
+
+;; --- Authorization Code Flow ---
+
+(defn auth-code [{:keys [client-id client-secret redirect-uri scope port preset] :as opts}]
+  (let [con (System/console)
+        {:keys [authorize-url token-url default-scope] :as endpoints} (resolve-endpoints opts)
+        _ (when-not (and authorize-url token-url)
+            (throw (e/invalid-arguments
+                    "Specify issuer=<url>, -p <preset>, or authorize-url=<url> token-url=<url>"
+                    {:opts (dissoc opts :args :summary)})))
+        port (or (first port) 9180)
+        client-id (or (first client-id) (and con (.readLine con "%s" (into-array ["client-id: "]))))
+        client-secret (or (first client-secret) (and con (.readLine con "%s" (into-array ["client-secret: "]))))
+        redirect-uri (or (first redirect-uri) (format "http://localhost:%d/oauth2/callback" port))
+        scope (or (first scope) default-scope "openid")
+        pipe (chan)
+        preset-name (first preset)]
+
+    (println
+     (->> [""
+           "------------------------------------------------------------"
+           (str "Authorization Code Flow" (when preset-name (str " (" preset-name ")")))
+           (str "Listening on " redirect-uri)
+           ""
+           "Open the following URL in your browser:"
+           "------------------------------------------------------------"
+           ""
+           (str authorize-url "?"
+                (->> {:client_id client-id
+                      :response_type "code"
+                      :redirect_uri (url-encode redirect-uri)
+                      :scope (url-encode scope)
+                      :state "12345"}
+                     (map (fn [[k v]] (str (name k) "=" v)))
+                     (str/join "&")))
+           ""]
+          (str/join \newline)))
+    (let [server (run-server (callback-app token-url client-id client-secret redirect-uri scope pipe)
+                             {:host "0.0.0.0" :port port})
+          result (<!! pipe)]
+      (server)
+      (yc/add-extra-fields [result]
+                           :issuer (or (first (:issuer opts)) preset-name)))))
+
+;; --- Client Credentials Flow ---
+
+(defn auth-client [{:keys [client-id client-secret scope preset] :as opts}]
+  (let [con (System/console)
+        {:keys [token-url]} (resolve-endpoints opts)
+        _ (when-not token-url
+            (throw (e/invalid-arguments
+                    "Specify issuer=<url>, -p <preset>, or token-url=<url>"
+                    {:opts (dissoc opts :args :summary)})))
+        client-id (or (first client-id) (and con (.readLine con "%s" (into-array ["client-id: "]))))
+        client-secret (or (first client-secret) (and con (.readLine con "%s" (into-array ["client-secret: "]))))
+        scope (or (first scope) (:default-scope (get presets (first preset))) "openid")]
+
+    (log/debug "Client Credentials:" token-url)
+    (let [result (-> @(http/post token-url
+                                {:form-params {:client_id client-id
+                                               :client_secret client-secret
+                                               :grant_type "client_credentials"
+                                               :scope scope}})
+                     :body
+                     (json->edn))]
+      (yc/add-extra-fields [result]
+                           :issuer (or (first (:issuer opts)) (first preset))))))
+
+;; --- Legacy Azure handler (delegate to auth-code) ---
+
+(defn auth-azure [{:keys [tenant] :as opts}]
+  (let [tenant (or (seq tenant)
+                   (let [con (System/console)]
+                     (when con
+                       [(.readLine con "%s" (into-array ["tenant: "]))])))]
+    (when-not (seq tenant)
+      (throw (e/invalid-arguments "tenant is required for azure preset" {})))
+    (auth-code (assoc opts :preset ["azure"] :tenant tenant))))
+
+;; --- Usage / Options / Routes ---
+
 (defn usage [opts]
   (let [{:keys [summary help-all]} (if (map? opts) opts {:summary opts :help-all true})]
     (->> (concat
-          ["Usage: auth <idp-service> [key-values]"
+          ["Usage: auth <flow> [options] [key=value ...]"
            ""
-           "Run OAuth2 authorization code flow"
+           "Run OAuth2 flows to obtain tokens"
            ""
            "Options:"
            ""
            summary
            ""]
           (when help-all
-            ["Resources:"
+            ["Flows:"
              ""
-             "  - azure key=val       ..."
+             "  code      Authorization Code Flow (browser-based)"
+             "  client    Client Credentials Flow"
+             "  azure     Azure Entra preset (= code -p azure)"
              ""
              "Keys:"
-             "  azure"
-             "    - tenant"
+             "  Common:"
+             "    - issuer         OIDC issuer URL (auto-discovers endpoints)"
+             "    - authorize-url  Authorization endpoint (explicit)"
+             "    - token-url      Token endpoint (explicit)"
              "    - client-id"
              "    - client-secret"
-             "    - redirect-uri    (default: http://localhost:9180/oauth2/callback)"
-             "    - scope           (default: https://graph.microsoft.com/.default)"
+             "    - redirect-uri   (default: http://localhost:9180/oauth2/callback)"
+             "    - scope          (default: openid)"
+             ""
+             "  Azure preset (-p azure):"
+             "    - tenant         Azure AD tenant ID"
              ""])
-          ["Example:"
+          ["Examples:"
            ""
-           "# Azure Entra authorization flow"
-           "  > yaac auth azure tenant=your-tenant client-id=zzz client-secret=zzz"
+           "# Authorization code flow with OIDC discovery"
+           "  > yaac auth code issuer=https://my-idp client-id=xxx client-secret=xxx"
+           ""
+           "# Authorization code flow with Azure preset"
+           "  > yaac auth code -p azure tenant=my-tenant client-id=xxx client-secret=xxx"
+           ""
+           "# Client credentials flow"
+           "  > yaac auth client issuer=https://my-idp client-id=xxx client-secret=xxx"
+           ""
+           "# Azure (legacy shorthand)"
+           "  > yaac auth azure tenant=my-tenant client-id=xxx client-secret=xxx"
            ""])
          (str/join \newline))))
 
-(def options [["-p" "--port <number>" "Listen port to receive redirected response from IdP"
+(def options [["-p" "--preset NAME" "Preset (azure)"]
+              ["--port" "--port PORT" "Listen port for callback (default: 9180)"
                :parse-fn parse-long]])
 
-(defn app [tenant client-id client-secret redirect-uri scope pipe]
-  (rh/ring-handler
-   (rh/router [["/oauth2/callback" {:get (fn [{:keys [params] :as req}]
-                                           (let [{:strs [code state]} params]
-                                             (-> @(http/post (str "https://login.microsoftonline.com/" tenant "/oauth2/v2.0/token")
-                                                            (doto {:form-params {:client_id client-id
-                                                                            :scope scope
-                                                                            :code code
-                                                                            :redirect_uri redirect-uri
-                                                                            :grant_type "authorization_code"
-                                                                            :client_secret client-secret}}
-                                                              prn))
-                                                 :body
-                                                 (json->edn)
-                                                 (as-> x (do (>!! pipe x) x)
-                                                   (assoc {:status 200} :body (edn->json :snake x))))))}]])
-   ;; (reitit.ring/routes
-   ;;  (reitit.ring/create-default-handler))
-   {:executor reitit.interceptor.sieppari/executor
-    :interceptors [(reitit.http.interceptors.parameters/parameters-interceptor)]}))
-
-
-
-(defn auth-azure [{:keys [tenant client-id client-secret redirect-uri scope port]}]
-  (let [con (System/console)]
-    (let [port (or (first port) 9180)
-          tenant (or (first tenant) (.readLine con "%s" (into-array ["tenant: "])))
-          client-id (or (first client-id) (.readLine con "%s" (into-array ["client id: "])))
-          client-secret (or (first client-secret) (.readLine con "%s" (into-array ["client secret: "])))
-          redirect-uri (or (first redirect-uri) (format "http://localhost:%d/oauth2/callback" port))
-          scope (or (first scope) "https://graph.microsoft.com/.default")
-          pipe (chan)]
-      
-      (println
-       (->> [""
-             "------------------------------------------------------------"
-             (str "Start authorization code flow with Azure. Listening on" )
-             (str "-- " redirect-uri)
-             (str "as callback URL registerred on Azure")
-             ""
-             (str "Go to the following site with copying to your browser.")
-             "------------------------------------------------------------"
-             ""
-             (str "https://login.microsoftonline.com/" tenant "/oauth2/v2.0/authorize?"
-                  (->> {:client_id client-id
-                        :response_type "code"
-                        :redirect_uri (url-encode redirect-uri)
-                        :scope (url-encode scope)
-                        :state "12345"}
-                       (map (fn [[k v] ] (str (name k) "=" v)))
-                       (str/join "&")))
-             ""]
-            (str/join \newline)))
-      (let [token-client (run-server (app tenant client-id client-secret redirect-uri scope pipe) {:host "0.0.0.0" :port 9180})
-            result (<!! pipe)]
-        (token-client)
-        (yc/add-extra-fields [result] :tenant tenant)))))
-
+(def result-fields [[:extra :issuer]
+                    :token-type
+                    :expires-in
+                    :scope])
 
 (def route
   ["auth" {:options options
            :usage usage}
    ["" {:help true}]
+   ;; Authorization Code Flow
+   ["|code" {:help true}]
+   ["|code|{*args}" {:fields result-fields
+                     :handler auth-code}]
+   ;; Client Credentials Flow
+   ["|client" {:help true}]
+   ["|client|{*args}" {:fields result-fields
+                       :handler auth-client}]
+   ;; Legacy Azure shorthand
    ["|azure" {:help true}]
-   ["|azure|{*args}" {:fields [[:extra :tenant]
-                               :token-type
-                               :expires-in
-                               :scope]
+   ["|azure|{*args}" {:fields result-fields
                       :handler auth-azure}]])
